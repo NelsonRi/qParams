a5804b1af49787554e867a1c3306a2b83cc81e2b
Ronald Nelson <nelsonri21@gmail.com>
2015-12-23 14:48:19 -0500

    Fixed variable mixup

--- qParams.js
+++ qParams.js
@@ -2,7 +2,7 @@ function getQVar(argv) {
 	var q = window.location.search.substring(1);
 	var string = q.split('&');
 	for (var idx = 0; idx < string.length; idx++) {
-		var kv = kv[idx].split('=');
+		var kv = string[idx].split('=');
 		if (decodeURIComponent(kv[0]) == argv) {
 			return decodeURIComponent(kv[1]);
 		}
